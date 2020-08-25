c2d86f59284c5641fa9630466dfbf40b66d22af8
Mithun Ganeasn <pagidala9@gmail.com>
2020-08-25 18:28:29 +0530

    Nitya-Inc

---
+++ src/Question1.java
@@ -0,0 +1,47 @@
+public class Question1 {
+	
+    public static class Class1{
+    	Class2 b;
+
+		public Class1(Class2 b) {
+			super();
+			this.b = b;
+		}
+    }
+    
+    public static class Class2{
+    	int c;
+
+		public Class2(int c) {
+			super();
+			this.c = c;
+		}
+ 
+    }
+    
+    public static class Class3{
+        Class4 c;
+
+		public Class3(Class4 c) {
+			super();
+			this.c = c;
+		}
+        
+    }
+
+    public static class Class4{
+   int a;
+   public Class4(int a) {
+	super();
+	this.a = a;
+}
+
+    }
+    
+    public static void main(String [] args){
+    Class1 a = new Class1(new Class2(23));
+      Class3 b = new Class3(new Class4(43));
+        a.b.c = b.c.a;
+        
+    }
+}
---
+++ src/Question2.java
@@ -0,0 +1,82 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.time.Month;
+import java.util.*;
+
+
+public class Question2 {
+    private File directory;
+    private int maxLineLength;
+    private Map<Integer,Integer> groupData;
+    
+    public Question2(File directory){
+    	
+    	if(!directory.isDirectory()) {
+            System.out.println("Given Path is not a Directory");
+            System.exit(-1);
+        }
+    	
+        this.directory = directory;
+        this.groupData  = new TreeMap<>();
+        this.maxLineLength = 15;
+        
+       
+    }
+ 
+   
+    
+    private void printData(){
+        System.out.print("MONTH");
+        for(int j=0;j<maxLineLength-"count".length();j++) System.out.print(' ');
+        System.out.println("FILE COUNT");
+        System.out.println();
+        for(int i = 1;i<=13;i++){
+            if(groupData.containsKey(i)){
+            	String month=Month.of(i).name();
+            	System.out.print(month);
+            	
+            	for(int j=0;j<maxLineLength-month.length();j++) System.out.print(' ');
+            	
+            	System.out.println(groupData.get(i));
+                
+            }
+        }
+    }
+    
+    
+    private void loadData() throws IOException {
+        for(File file : directory.listFiles()){
+            if(file!=null && file.isFile()){
+
+            	    BasicFileAttributes attrs;
+        		    attrs = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+        		    FileTime time = attrs.creationTime(); 		    
+        		    long createdDateInMillis= time.toMillis();
+        		    Calendar cal = Calendar.getInstance();
+        	        cal.setTimeInMillis(createdDateInMillis);
+        	        int month=cal.get(Calendar.MONTH)+1;
+                    groupData.put(month,groupData.getOrDefault(month,0)+1);
+            }
+        }
+    }
+ 
+    public static void main(String[] args){
+    	System.out.println("Paste the link to directory");
+    	Scanner scan = new Scanner(System.in);
+    	String directoryPath =  scan.nextLine();        
+        File directory = new File(directoryPath);
+        Question2 question2 = new Question2(directory);
+    
+        try {
+            question2.loadData();
+            question2.printData();
+        } catch (Exception e) 
+        {
+        	   e.printStackTrace();
+        }
+    }
+}
